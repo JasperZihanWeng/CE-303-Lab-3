83f36d2f11a7f8bbfab399040d729ccd68b08fb1
JasperZihanWeng <jasperweng@outlook.com>
2026-06-01 03:28:40 -0500

    first untested try

---
+++ accelerator.v
@@ -0,0 +1,55 @@
+module accelerator(IN1, IN2, IN3, W1, W2, W3, CLK, RSTB, OUT1, OUT2, OUT3, OUT4, OUT5, OUT6, OUT7, OUT8, OUT9);
+
+    //input output decleration
+    input signed [7:0] IN1;
+    input signed [7:0] IN2;
+    input signed [7:0] IN3;
+
+    input signed [7:0] W1;
+    input signed [7:0] W2;
+    input signed [7:0] W3;
+
+    input CLK;
+    input RSTB;
+
+    output signed [19:0] OUT1;
+    output signed [19:0] OUT2;
+    output signed [19:0] OUT3;
+    output signed [19:0] OUT4;
+    output signed [19:0] OUT5;
+    output signed [19:0] OUT6;
+    output signed [19:0] OUT7;
+    output signed [19:0] OUT8;
+    output signed [19:0] OUT9;
+
+    
+    //internal wires 
+    wire [7:0] IN1_R;
+    wire [7:0] IN1_RR;
+    wire [7:0] IN2_R;
+    wire [7:0] IN2_RR;
+    wire [7:0] IN3_R;
+    wire [7:0] IN3_RR;
+
+    wire [7:0] W1_R;
+    wire [7:0] W1_RR;
+    wire [7:0] W2_R;
+    wire [7:0] W2_RR;
+    wire [7:0] W3_R;
+    wire [7:0] W3_RR;
+
+
+    //start doing the modules 
+    mac_unit UNIT_1(.W(W1), .IN(IN1), .CLK(CLK), .RSTB(RSTB), .W_R(W1_R), .IN_R(IN1_R), .OUT(OUT1));
+    mac_unit UNIT_2(.W(W2), .IN(IN1_R), .CLK(CLK), .RSTB(RSTB), .W_R(W2_R), .IN_R(IN1_RR), .OUT(OUT2));
+    mac_unit UNIT_3(.W(W3), .IN(IN1_RR), .CLK(CLK), .RSTB(RSTB), .W_R(W3_R), .IN_R(), .OUT(OUT3));
+
+    mac_unit UNIT_4(.W(W1_R), .IN(IN2), .CLK(CLK), .RSTB(RSTB), .W_R(W1_RR), .IN_R(IN2_R), .OUT(OUT4));
+    mac_unit UNIT_5(.W(W2_R), .IN(IN2_R), .CLK(CLK), .RSTB(RSTB), .W_R(W2_RR), .IN_R(IN2_RR), .OUT(OUT5));
+    mac_unit UNIT_6(.W(W3_R), .IN(IN2_RR), .CLK(CLK), .RSTB(RSTB), .W_R(W3_RR), .IN_R(), .OUT(OUT6));
+
+    mac_unit UNIT_7(.W(W1_RR), .IN(IN3), .CLK(CLK), .RSTB(RSTB), .W_R(), .IN_R(IN3_R), .OUT(OUT7));
+    mac_unit UNIT_8(.W(W2_RR), .IN(IN3_R), .CLK(CLK), .RSTB(RSTB), .W_R(), .IN_R(IN3_RR), .OUT(OUT8));
+    mac_unit UNIT_9(.W(W3_RR), .IN(IN3_RR), .CLK(CLK), .RSTB(RSTB), .W_R(), .IN_R(), .OUT(OUT9));
+
+endmodule
---
+++ mac_unit.v
@@ -0,0 +1,64 @@
+module MAC_Unit(W, IN, CLK, RSTB, W_R, IN_R, OUT);
+
+    //intput outputs 
+    input signed [7:0] W;
+    input signed [7:0] IN;
+
+    input CLK;
+    input RSTB;
+
+    output reg signed [7:0] W_R;
+    output reg signed [7:0] IN_R;
+
+    output reg signed [19:0] OUT;
+
+    
+
+    //Intialize registers
+    //this is the accumulator
+    reg [19:0] Accumulator = 19'b0;
+
+    //this will count how may clocks has actually happened
+    reg [3:0] CLK_COUNT = 4'b0000;
+
+
+    
+    always @(posedge CLK or negedge RSTB) begin
+        
+        //this handles the reset logic
+        if (RSTB == 1'b0) begin
+            CLK_COUNT = 4'b0000;
+            Accumulator = 19'b0;
+        end
+        else begin
+
+            //this just updates it on the next clock cycle
+            W_R = W;
+            IN_R = IN;
+
+            //this handles the accumulator logic
+            if (CLK_COUNT != 4'b1101) begin
+                Accumulator = Accumulator + (W * IN);
+            end
+            else begin
+                Accumulator = Accumulator;
+            end
+
+            //assign OUT
+            OUT = Accumulator;
+
+            //this updates the CLK_COUNT
+            if (CLK_COUNT != 4'b1101) begin
+                CLK_COUNT = CLK_COUNT + 1;
+            end
+            else begin
+                CLK_COUNT = CLK_COUNT;
+            end
+            
+        end
+    end
+
+
+
+    
+endmodule
